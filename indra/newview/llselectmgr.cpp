96cc24f18a4a500b82cd8a6993234e95cb415ba0
Trish_sl <sl.trish.ace@gmail.com>
2026-05-08 22:47:00 -0400

    FIX - MOAP selection should not show highlights outside of build mode

--- indra/newview/llselectmgr.cpp
+++ indra/newview/llselectmgr.cpp
@@ -6942,6 +6942,10 @@ void LLSelectMgr::renderSilhouettes(bool for_hud)
                 LLViewerObject* objectp = node->getObject();
                 if (!objectp)
                     continue;
+                // When media-on-a-prim grabs focus outside build mode, avoid drawing
+                // build-style selection highlight for that focused object.
+                if (!LLToolMgr::instance().inBuildMode() && focus_item_id.notNull() && objectp->getID() == focus_item_id)
+                    continue;
 
                 if (objectp->mDrawable
                     && objectp->mDrawable->getVOVolume()
@@ -9331,4 +9335,3 @@ bool LLCheckIdenticalFunctor<class LLFace *>::same(class LLFace* const & a, clas
     (void)tolerance;                                                                \
     return a == b;                                                                  \
 }
-
