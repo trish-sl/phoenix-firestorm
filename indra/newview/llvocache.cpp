55cdb9e41b98ce307430ed4a5c60bd8a4a6c00d6
Trish_sl <sl.trish.ace@gmail.com>
2026-06-27 05:00:32 -0400

    FIX - Adjust texture-related memory reporting

--- indra/newview/llvocache.cpp
+++ indra/newview/llvocache.cpp
@@ -484,7 +484,7 @@ void LLVOCacheEntry::updateDebugSettings()
     // <FS:Beq> FIRE-32688 Area search and other visibility issues
     // If this machine has limited RAM, then restore the LL defaults.
     // So long as we have at least 8GB of RAM, then we will use our values.
-    if( LLMemory::getAvailableMemKB() * KB_to_MB < 8096 )
+    if( LLMemory::getMaxMemKB() * KB_to_MB < 8096 )
     {
         if( (U32)low_mem_bound_MB > 768 )
         {
@@ -492,7 +492,7 @@ void LLVOCacheEntry::updateDebugSettings()
         }
         if( (U32)high_mem_bound_MB > 2048 )
         {
-            gSavedSettings.setU32("SceneLoadLowMemoryBound", 2048);
+            gSavedSettings.setU32("SceneLoadHighMemoryBound", 2048);
         }
     }
     // </FS:Beq>
