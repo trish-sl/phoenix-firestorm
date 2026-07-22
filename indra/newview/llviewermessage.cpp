84636cc1a9d9059809d2afe1462ced78ac8cc075
Trish_sl <sl.trish.ace@gmail.com>
2026-07-21 21:14:15 -0400

    FIX - Preserve capabilities during region refresh

--- indra/newview/llviewermessage.cpp
+++ indra/newview/llviewermessage.cpp
@@ -3864,7 +3864,7 @@ void process_teleport_finish(LLMessageSystem* msg, void**)
     gAgent.setTeleportState( LLAgent::TELEPORT_MOVING );
     gAgent.setTeleportMessage(LLAgent::sTeleportProgressMessages["contacting"]);
 
-    LL_DEBUGS("CrossingCaps") << "Calling setSeedCapability(). Seed cap == "
+    LL_DEBUGS("CrossingCaps") << "Calling refreshSeedCapabilities(). Seed cap == "
             << seedCap << LL_ENDL;
 
     // <FS:TJ> Fixes OpenSim race condition on grid change not having updated Grid Info yet
@@ -3874,7 +3874,7 @@ void process_teleport_finish(LLMessageSystem* msg, void**)
             { LLAppViewer::instance()->updateNameLookupUrl(regionp); });
     }
     // </FS:TJ>
-    regionp->setSeedCapability(seedCap);
+    regionp->refreshSeedCapabilities(seedCap);
 
     // Don't send camera updates to the new region until we're
     // actually there...
@@ -4245,9 +4245,9 @@ void process_crossed_region(LLMessageSystem* msg, void**)
     //LLViewerRegion* regionp = LLWorld::getInstance()->addRegion(region_handle, sim_host);
     LLViewerRegion* regionp = LLWorld::getInstance()->addRegion(region_handle, sim_host, region_size_x, region_size_y);
 // </FS:CR> Aurora Sim
-    LL_DEBUGS("CrossingCaps") << "Calling setSeedCapability from process_crossed_region(). Seed cap == "
+    LL_DEBUGS("CrossingCaps") << "Calling refreshSeedCapabilities from process_crossed_region(). Seed cap == "
             << seedCap << LL_ENDL;
-    regionp->setSeedCapability(seedCap);
+    regionp->refreshSeedCapabilities(seedCap);
 }
 
 
@@ -6557,7 +6557,7 @@ static void try_recover_inventory_connection(const std::string& notification_id)
         if (!seed_url.empty())
         {
             LL_WARNS("Inventory") << "Refreshing region seed capabilities for recovery." << LL_ENDL;
-            region->setSeedCapability(seed_url);
+            region->refreshSeedCapabilities(seed_url);
         }
         else
         {
