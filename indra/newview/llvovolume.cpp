4a0d9ca69fff250098f03272e7d52c53f17ff490
Trish_sl <sl.trish.ace@gmail.com>
2026-08-28 23:59:38 -0400

    PERF - Pause offscreen texture animation without desynchronizing state

--- indra/newview/llvovolume.cpp
+++ indra/newview/llvovolume.cpp
@@ -349,6 +349,7 @@ LLVOVolume::LLVOVolume(const LLUUID &id, const LLPCode pcode, LLViewerRegion *re
     // NaCl End
 {
     mTexAnimMode = 0;
+    mTextureAnimPrepared = false;
     mRelativeXform.setIdentity();
     mRelativeXformInvTrans.setIdentity();
 
@@ -537,6 +538,7 @@ U32 LLVOVolume::processUpdateMessage(LLMessageSystem *mesgsys,
                     }
                 }
                 mTexAnimMode = 0;
+                mTextureAnimPrepared = false;
 
                 mTextureAnimp->unpackTAMessage(mesgsys, block_num);
             }
@@ -561,6 +563,7 @@ U32 LLVOVolume::processUpdateMessage(LLMessageSystem *mesgsys,
                     gPipeline.markTextured(mDrawable);
                     mFaceMappingChanged = true;
                     mTexAnimMode = 0;
+                    mTextureAnimPrepared = false;
                 }
             }
 
@@ -739,6 +742,7 @@ U32 LLVOVolume::processUpdateMessage(LLMessageSystem *mesgsys,
                     }
                 }
                 mTexAnimMode = 0;
+                mTextureAnimPrepared = false;
                 mTextureAnimp->unpackTAMessage(*dp);
             }
             else if (mTextureAnimp)
@@ -760,6 +764,7 @@ U32 LLVOVolume::processUpdateMessage(LLMessageSystem *mesgsys,
                 gPipeline.markTextured(mDrawable);
                 mFaceMappingChanged = true;
                 mTexAnimMode = 0;
+                mTextureAnimPrepared = false;
             }
 
             if (value & 0x400)
@@ -874,8 +879,25 @@ void LLVOVolume::onDrawableUpdateFromServer()
 
 void LLVOVolume::animateTextures()
 {
-    if (!mDead && mDrawable) // <FS:Beq/> FIRE-34601 - bugsplat accessing null drawable.
+    if (mDead || !mDrawable || !mTextureAnimp || mDrawable->getNumFaces() == 0)
     {
+        mTextureAnimWasVisible = false;
+        return;
+    }
+
+    const bool visible = isVisible();
+    const bool became_visible = visible && !mTextureAnimWasVisible;
+    mTextureAnimWasVisible = visible;
+
+    // Prepare the texture matrix and render batch once even while off-screen,
+    // then avoid ongoing animation work until the object is visible.
+    if (!mTextureAnimPrepared || visible)
+    {
+        if (became_visible)
+        {
+            updateTextureVirtualSize();
+        }
+        mTextureAnimPrepared = true;
         shrinkWrap();
         F32 off_s = 0.f, off_t = 0.f, scale_s = 1.f, scale_t = 1.f, rot = 0.f;
         S32 result = mTextureAnimp->animateTextures(off_s, off_t, scale_s, scale_t, rot);
